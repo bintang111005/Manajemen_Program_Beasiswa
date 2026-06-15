6b231ed9aeec9fe892da548ebf1f3b8a3855aef3
bintang111005 <bintang1112005@gmail.com>
2026-06-16 00:36:28 +0700

    create public landing page

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -12,62 +12,17 @@ Program Beasiswa
 
 <p>
 
-Temukan peluang terbaik.
+Platform pengelolaan program beasiswa
 
 </p>
 
 <a
-class="btn btn-main">
+href="/scholarships"
+class="btn btn-success">
 
 Daftar Sekarang
 
 </a>
-
-</div>
-
-<br>
-
-<div class="row">
-
-<div class="col-md-6">
-
-<div class="card p-4">
-
-<h3>
-
-🎓 Akademik
-
-</h3>
-
-<p>
-
-Prestasi Nilai & Riset
-
-</p>
-
-</div>
-
-</div>
-
-<div class="col-md-6">
-
-<div class="card p-4">
-
-<h3>
-
-🏆 Non Akademik
-
-</h3>
-
-<p>
-
-Minat & Bakat
-
-</p>
-
-</div>
-
-</div>
 
 </div>
 

ff385faf7b457fc55348ac485f086ef98938a2b1
bintang111005 <bintang1112005@gmail.com>
2026-06-16 00:11:36 +0700

    buar layout utama

---
+++ resources/views/layouts/app.balde.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Manajemen Program Beasiswa</title>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1">
+
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-dark bg-primary">
+
+<div class="container">
+
+<a class="navbar-brand"
+href="/">
+
+Manajemen Program Beasiswa
+
+</a>
+
+</div>
+
+</nav>
+
+<div class="container mt-4">
+
+@yield('content')
+
+</div>
+
+</body>
+
+</html>
